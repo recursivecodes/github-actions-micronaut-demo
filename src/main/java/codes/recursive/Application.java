8791ba7c8fe2e907d2123ec85bd9bdb6a75f2c0e
recursivecodes <toddraymondsharp@gmail.com>
2020-04-08 14:16:47 -0400

    move password into env var/secret

--- src/main/java/codes/recursive/Application.java
+++ src/main/java/codes/recursive/Application.java
@@ -7,6 +7,10 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
     public static void main(String[] args) {
         System.setProperty("oracle.jdbc.fanEnabled", "false");
-        Micronaut.run(Application.class);
+        Micronaut
+                .build(new String[]{})
+                .mainClass(Application.class)
+                .environmentPropertySource(false)
+                .start();
     }
 }
--- src/test/groovy/codes/recursive/HelloControllerSpec.groovy
+++ src/test/groovy/codes/recursive/HelloControllerSpec.groovy
@@ -4,7 +4,6 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.annotation.MicronautTest
-import spock.lang.Specification
 
 import javax.inject.Inject
 
